d4ca18672125dc683a44c6954baeeab6ec907ea2
evyz <endermainYT2502@gmail.com>
2023-04-26 18:10:25 +0300

    add core to wrapper

--- src/evyz@react-forms/wrapper/wrapper.js
+++ src/evyz@react-forms/wrapper/wrapper.js
@@ -1,7 +1,26 @@
 import React from "react";
 import "./wrapper.css";
 
-const Wrapper = ({ children, isDarkMode }) => {
+const core = class {
+  constructor() {
+    this._isActive = false
+  } 
+  activeWorkspace = function () {
+    this._isActive = !this._isActive
+    return this._isActive
+  }
+
+  findWidget(args) {
+    if (!this._isActive) return
+    console.log(args)
+  }     
+}
+
+const Wrapper = ({ children, isDarkMode, useCoreConsole }) => {
+  if (useCoreConsole) {
+    window.core = new core()
+  }
+
   return (
     <div
       className={"system_wrapper " + `${isDarkMode ? "system_mode-dark" : ""}`}
