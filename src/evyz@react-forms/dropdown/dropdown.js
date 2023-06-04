8d922957eafe0c8a2c1305e906b55fd483e9288e
Max <maks.fomichev.94@mail.ru>
2023-06-04 18:11:46 +0400

    upd

--- src/evyz@react-forms/dropdown/dropdown.js
+++ src/evyz@react-forms/dropdown/dropdown.js
@@ -1,27 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./dropdown.css";
 
 const DropdownItem = ({ option, setOption, len, onClickItem }) => {
   return (
     <div
-
-      onClick={() => setOption(option)}
       className="system_dropdown_option dropdown_value"
       onClick={() => {
-        onClickItem && onClickItem()
-        setOption(option)
+        onClickItem && onClickItem();
+        setOption(option);
       }}
       style={{
         borderBottom: len - 1 === option.id ? "none" : "1px solid gray",
       }}
-      className='system_dropdown_option'
     >
       {option.value}
     </div>
   );
 };
 
-const Dropdown = ({ currOption, setCurrOption, options, defaultValue }) => {
+const Dropdown = ({
+  styleRules,
+  rules,
+  options,
+  defaultValue,
+  label,
+  currOption,
+  setCurrOption,
+  onClickHandler,
+}) => {
   const [isOpened, setIsOpened] = useState(false);
 
   // ТИПА ИЗМЕНЕНИЯ ЧТОБЫ НАЕБАТЬ ПУЛЛ РЕКВЕСТ УХАХАХАХ
@@ -39,52 +45,7 @@ const Dropdown = ({ currOption, setCurrOption, options, defaultValue }) => {
           } `}
         ></button>
       </div>
-      <div
-        style={{ visibility: isOpened ? "initial" : "hidden" }}
-        className="dropdown_seperator"
-      />
-      <div
-        style={{ display: isOpened ? "flex" : "none" }}
-        className="dropdown_values_wrapper"
-      >
-        {options.map((option) => (
-          <DropdownItem option={option} setOption={setCurrOption} />
-        ))}
-        <div
-          className={`system_dropdown_ceiling`}
-          onClick={() => setIsOpened(!isOpened)}
-        >
-          <span>
-            {currOption === null
-              ? !defaultValue
-                ? label
-                : defaultValue
-              : currOption.value}
-          </span>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <div
-              style={{ zIndex: 2, cursor: "pointer" }}
-              onClick={() => {
-                onClickHandler && onClickHandler({value: null}) 
-                setCurrOption(null)
-              }}
-            >
-              x
-            </div>
-            <div
-              style={{ marginLeft: 10 }}
-              className={`system_dropdown_arrow ${
-                isOpened ? `arrow_up` : `arrow_down`
-              }`}
-            />
-          </div>
-        </div>
-      </div>
+
       <div
         style={{
           display: isOpened ? "" : "none",
@@ -102,7 +63,7 @@ const Dropdown = ({ currOption, setCurrOption, options, defaultValue }) => {
               setOption={setCurrOption}
               len={options.length}
               onClickItem={() => {
-                 onClickHandler && onClickHandler({value: option}) 
+                onClickHandler && onClickHandler({ value: option });
               }}
             />
           ))}
