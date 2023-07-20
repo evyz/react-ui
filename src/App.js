0548674ba44a26c17fd13e3bfdbe9289735e38e0
evyz <endermainYT2502@gmail.com>
2023-07-20 18:29:08 +0300

    added grid settings

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 import { useEffect, useState } from "react";
-import { StackAlerts } from "./evyz@react-forms-confirmed/index";
 import Wrapper from "./evyz@react-forms/wrapper/wrapper";
+import Grid from "./evyz@react-forms-confirmed/grid/grid";
 
 function App() {
-  const [notif, setNotifs] = useState([]);
+
+  const [rows, setRows] = useState([])
+  const [page, setPage] = useState(0)
+  const [limit ,setLimit] = useState(2)
 
   useEffect(() => {
-    setNotifs([
-      { id: 1, title: "123", message: "123" },
-      { id: 1, title: "123", message: "123" },
-    ]);
+    setRows([
+      {id: 1, name: "Test", size: "123", isConfirmed: true},
+      {id: 2, name: "Tes2232", size: "123", isConfirmed: false},
+      {id: 3, name: "Tes2132131t", size: "123", isConfirmed: true},
+      {id: 4, name: "Test123", size: "123", isConfirmed: true},
+      {id: 5, name: "Test123", size: "123", isConfirmed: true},
+    ])
+  }, [])
 
-    setTimeout(() => {
-      setNotifs([
-        { id: 1, title: "123", message: "123" },
-        { id: 1, title: "123", message: "123" },
-        { id: 1, title: "111111111111", message: "123" },
-        { id: 1, title: "111111111111", message: "123" },
-      ]);
-    }, 2000);
-  }, []);
-
-  console.log("notif =>", notif);
 
   return (
     <Wrapper>
-      <StackAlerts
-        state={{ value: notif }}
-        gui={{ placement: { x: "left", y: "top" } }}
-      ></StackAlerts>
+      <Grid state={{value: rows}} gui={{fields: [
+    { id: 1, key: "id" },
+    { id: 2, key: "name", thead: {style: {width: 200}} },
+    { id: 3, key: "size" },
+  ], page, limit}}></Grid>
     </Wrapper>
   );
 }
--- src/evyz@react-forms-confirmed/grid/grid.tsx
+++ src/evyz@react-forms-confirmed/grid/grid.tsx
@@ -1,18 +1,115 @@
+import * as React from "react";
 import { WidgetBaseOptions } from "..";
+import { usePaginationGrid } from "./pagination";
 
-
-
-interface GridOptions extends WidgetBaseOptions{
-    events: {
-        row: {
-            onClick: Function;
-        }
-        cell:{
-            onClick: Function;
-        }
-    }
-    gui:{
-        fields: [];
-        size: Number;
-    }
+interface FieldOptions {
+  id?: Number;
+  key?: string;
+  value?: any;
+  thead?: {
+    className?: string;
+    style?: React.CSSProperties;
+  };
+  tbody?: {
+    className?: string;
+    style?: React.CSSProperties;
+  };
+  events?: {
+    onClick?: Function;
+  };
 }
+
+interface FieldsOptions extends Array<FieldOptions> {}
+
+interface gui {
+  gui: {
+    fields: FieldsOptions;
+    thead?: {
+      className?: string;
+      style?: React.CSSProperties;
+    };
+    tbody?: {
+      className?: string;
+      style?: React.CSSProperties;
+    };
+  };
+}
+
+export interface GridOptions extends WidgetBaseOptions {
+  events?: {
+    row?: {
+      onClick?: Function;
+    };
+  };
+
+  state?: {
+    value?: [];
+  };
+}
+
+const Thead = (props: GridOptions) => {
+  return (
+    <thead>
+      {props?.gui?.fields &&
+        props?.gui?.fields?.length &&
+        props?.gui?.fields.map((field) => (
+          <th
+            style={field?.thead?.style}
+            className={`${field?.thead?.className}`}
+            key={`${field?.id ? field?.id : field?.key}`}>
+            {field?.key}
+          </th>
+        ))}
+    </thead>
+  );
+};
+
+interface TBodyProps {
+  gui: {};
+  data: [];
+  onClickRow;
+  onClickCell;
+}
+
+const Tbody = ({}) => {
+  return (
+    <tbody>
+      {data &&
+        data?.length &&
+        data.map((row) => (
+          <tr onClick={onClickRow}>
+            {gui?.fields &&
+              gui?.fields?.length &&
+              gui?.fields.map((field) => (
+                <th
+                  onClick={(e) =>
+                    onClickCell(e, {
+                      id: field?.id,
+                      key: field?.key,
+                      value: row[field?.key ? field?.key : ""],
+                    })
+                  }>
+                  {row[field?.key ? field?.key : ""]}
+                </th>
+              ))}
+          </tr>
+        ))}
+    </tbody>
+  );
+};
+
+const Grid = (props: GridOptions) => {
+  const {
+    data,
+    events: { onClickRow, onClickCell },
+  } = usePaginationGrid(props);
+  return (
+    <div>
+      <table>
+        <Thead gui={{ fields: props?.gui?.fields }} />
+      </table>
+    </div>
+  );
+};
+
+export default Grid;
