fecd51d289ae44d534ba7c90224138d140acc167
evyz <endermainYT2502@gmail.com>
2023-08-20 14:32:09 +0300

    rework interface grid structure + first render grid

--- src/components/grid/src/calendar.types.ts
+++ src/components/grid/src/calendar.types.ts
@@ -1,30 +1,32 @@
 import React from "react";
-import { WidgetBaseOptions, WidgetBaseStyles } from "../../base.types";
+import { WidgetBaseEvents, WidgetBaseOptions, WidgetBaseStyles } from "../../base.types";
 
-export interface CellItem {
-    id?:Number
-    value?:any
+export interface CellItem extends WidgetBaseEvents {
+    id?: string
+    value?: string
     component?: React.ReactNode
 }
 
 export interface CellsItems extends Array<CellItem>{}
+export interface StateItems extends Array<any>{}
 
-export interface CalendarTheadInterface extends WidgetBaseStyles{
+export interface GridTheadInterface extends WidgetBaseStyles{
     cols?: CellsItems
 }
 
-export interface CalendarTbodyInterface extends WidgetBaseStyles{
+export interface GridTbodyInterface extends WidgetBaseStyles{
     cols?: CellsItems
 }
 
-export interface CalendarGuiInterface extends WidgetBaseStyles{
-    thead?: CalendarTheadInterface
-    tbody?: CalendarTbodyInterface
+export interface GridGuiInterface extends WidgetBaseStyles{
+    thead?: GridTheadInterface
+    tbody?: GridTbodyInterface
 }
 
 
-export interface GridOptions extends WidgetBaseOptions, CalendarGuiInterface{
+export interface GridOptions extends WidgetBaseOptions {
     state?: {
-        value?: CellsItems;
+        value?: StateItems
     }
+    gui?: GridGuiInterface
 }
